fbf1679dfb0ac2e867b6dfc8fe2b96e1fd65fa1c
Jenpoer <jennifer.poernomo@seagate.com>
2026-04-09 16:39:49 +0800

    Restructure LFU metadata
    Signed-off-by: Jennifer Poernomo <jennifer.poernomo@seagate.com>

--- src/eviction/ocf_lfu_structs.h
+++ src/eviction/ocf_lfu_structs.h
@@ -2,16 +2,25 @@
 
 #define __EVICTION_LFU_STRUCTS_H__
 
-/* Raw hit counter is currently set to 32 because LFU_MAX_FREQ is 32 buckets */
-typedef uint8_t ocf_lfu_hits_t;
+/* Raw hit counter */
+typedef uint16_t ocf_lfu_hits_t;
+#define LFU_HITS_BITS (sizeof(ocf_lfu_hits_t) * 8)
+
+#define LFU_FREQ_BITS \
+    ((LFU_MAX_FREQ) <= 1 ? 0 : (32 - __builtin_clz((LFU_MAX_FREQ) - 1)))
 
 struct ocf_lfu_meta {
-    uint32_t freq;          // Access frequency counter
-    uint32_t prev;          // For doubly linked list in freq bucket
-    uint32_t next;
-    bool clean;
-    ocf_part_id_t partition_id;
-    ocf_lfu_hits_t hits;
+    /* 1st 64-bit word */
+    uint64_t prev : OCF_CACHE_LINE_BITS;
+    uint64_t next : OCF_CACHE_LINE_BITS;
+    uint64_t clean : 1;
+    uint64_t freq : LFU_FREQ_BITS;
+    uint64_t unused : 1;
+
+    /* 2nd 64-bit word */
+    uint64_t hits : LFU_HITS_BITS;
+    uint64_t partition_id : 8;
+    uint64_t unused2 : (64 - LFU_HITS_BITS - 8);
 } __attribute__((packed));
 
 struct ocf_lfu_list {
